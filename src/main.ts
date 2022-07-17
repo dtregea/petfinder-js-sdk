8fd046ef1e1ca7526610d176efbcc2a1c10037c8
dtregea <daniel.tregea@gmail.com>
2022-07-17 12:19:32 -0400

    Axios instance will authenticate and retry requests when access token expires

--- src/main.ts
+++ src/main.ts
@@ -25,9 +25,15 @@ export class Client {
 
         this.http.interceptors.response.use((response: AxiosResponse) => {
             return response;
-        }, (error: AxiosError) => {
+        },  async (error: AxiosError) => {
             if (error.response && this.isProblemDetailsResponse(error.response)) {
-                return Promise.reject(new ProblemDetailsError(error.request, error.response));
+                if (error.response.status === 401) {
+                    const authResponse = await this.authenticate();
+                    error.config.headers.Authorization = `Bearer ${authResponse.data.access_token}`;
+                    return axios.request(error.config);
+                } else {
+                    return Promise.reject(new ProblemDetailsError(error.request, error.response));
+                }
             }
 
             return Promise.reject(error);
@@ -46,25 +52,18 @@ export class Client {
         return new Organization(this);
     }
 
-    public async authenticate(token?: string): Promise<void | AxiosResponse> {
-        let response;
-        let accessToken = token || this.config.token;
-
-        if (!accessToken) {
-            response = await this.http.post("/oauth2/token", {
-                // eslint-disable-next-line @typescript-eslint/camelcase
-                client_id: this.config.apiKey,
-                // eslint-disable-next-line @typescript-eslint/camelcase
-                client_secret: this.config.secret,
-                // eslint-disable-next-line @typescript-eslint/camelcase
-                grant_type: "client_credentials",
-            });
-            accessToken = response.data.access_token;
-        }
-
+    public async authenticate(): Promise<AxiosResponse> {
+        const response = await this.http.post("/oauth2/token", {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            client_id: this.config.apiKey,
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            client_secret: this.config.secret,
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            grant_type: "client_credentials",
+        });
+        const accessToken = response.data.access_token;
         this.config.token = accessToken;
         this.http.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-
         return response;
     }
 
